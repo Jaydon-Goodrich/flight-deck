136b440ec48652642db15e7366fa5629f922884c
Jaydon Goodrich <jaydongoodrich@gmail.com>
2021-02-19 23:25:40 -0700

    had

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,16 @@
 import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Head from 'next/head'
 import NavBar from '../components/Nav';
 
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>Flight-Deck Avionics</title>
+        <link rel="icon" href="/testlogo4.png" />
+      </Head>
       <NavBar />
       <Component {...pageProps} />
     </>
