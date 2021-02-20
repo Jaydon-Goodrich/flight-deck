231a504b70f52a87fba9fa06881ac767509eb6c6
Jaydon Goodrich <jaydongoodrich@gmail.com>
2021-02-19 23:23:37 -0700

    about info

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,11 @@
+import styles from '../styles/Home.module.css'
+
 
 const about = () => {
     return (
-        <div>
-            ABOUT
+        <div className={styles.container}>
+            <h2>About Flight-Deck Avionics</h2>
+            <p>Flight-Deck Avionics is Utah's premier FAA Certified Repair Station offering Avionics services and installation within the Salt Lake City/ Provo metropolitan area.  With fixed locations at SLC and PVU airports, and complete mobile services, we strive to offer the utmost in convenience to our clients.  The primary goal of Flight-Deck Avionics is to operate a successful business, providing cutting-edge aircraft maintenance, with a degree of safety that is unparalleled.  Honesty, value, and customer service are virtues by which we operate.  With superb technicians, backed by the worlds largest avionics suppliers, we believe that we've got what it takes to meet our goals.....  And yours. </p>
         </div>
     )
 }
---
+++ pages/services.js
@@ -0,0 +1,81 @@
+import styles from '../styles/Home.module.css'
+import Image from 'react-bootstrap/Image'
+import {Container, Row, Col} from 'react-bootstrap/'
+
+
+const services = () => {
+    return (
+        <div className={styles.container}>
+            <h2>Services</h2>
+
+            <Container>
+                <Row>
+                    <Col>
+                        <Image src="https://via.placeholder.com/150" roundedCircle />
+                    </Col>
+                    <Col>
+                        <Image src="https://via.placeholder.com/150" roundedCircle />
+                    </Col>
+                    <Col>
+                        <Image src="https://via.placeholder.com/150" roundedCircle />
+                    </Col>
+                    <Col>
+                        <Image src="https://via.placeholder.com/150" roundedCircle />
+                    </Col>
+                    <Col>
+                        <Image src="https://via.placeholder.com/150" roundedCircle />
+                    </Col>
+                    <Col>
+                        <Image src="https://via.placeholder.com/150" roundedCircle />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <Image src="https://via.placeholder.com/150" roundedCircle />
+                    </Col>
+                    <Col>
+                        <Image src="https://via.placeholder.com/150" roundedCircle />
+                    </Col>
+                    <Col>
+                        <Image src="https://via.placeholder.com/150" roundedCircle />
+                    </Col>
+                    <Col>
+                        <Image src="https://via.placeholder.com/150" roundedCircle />
+                    </Col>
+                    <Col>
+                        <Image src="https://via.placeholder.com/150" roundedCircle />
+                    </Col>
+                    <Col>
+                        <Image src="https://via.placeholder.com/150" roundedCircle />
+                    </Col>
+                    <Col>
+                        <Image src="https://via.placeholder.com/150" roundedCircle />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <Image src="https://via.placeholder.com/150" roundedCircle />
+                    </Col>
+                    <Col>
+                        <Image src="https://via.placeholder.com/150" roundedCircle />
+                    </Col>
+                    <Col>
+                        <Image src="https://via.placeholder.com/150" roundedCircle />
+                    </Col>
+                    <Col>
+                        <Image src="https://via.placeholder.com/150" roundedCircle />
+                    </Col>
+                    <Col>
+                        <Image src="https://via.placeholder.com/150" roundedCircle />
+                    </Col>
+                    <Col>
+                        <Image src="https://via.placeholder.com/150" roundedCircle />
+                    </Col>
+                </Row>
+            </Container>
+
+        </div>
+    )
+}
+
+export default services;
