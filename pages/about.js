3cb8abdc2d5265207215b5a0f3f75730d7f316a4
Jaydon Goodrich <jaydongoodrich@gmail.com>
2021-02-24 18:42:41 -0700

    services

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,29 @@
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.css';
+import { Container, Row, Col } from 'react-bootstrap';
+import Image from 'react-bootstrap/Image'
 
 
 const about = () => {
     return (
-        <div className={styles.container}>
-            <h2>About Flight-Deck Avionics</h2>
-            <p>Flight-Deck Avionics is Utah's premier FAA Certified Repair Station offering Avionics services and installation within the Salt Lake City/ Provo metropolitan area.  With fixed locations at SLC and PVU airports, and complete mobile services, we strive to offer the utmost in convenience to our clients.  The primary goal of Flight-Deck Avionics is to operate a successful business, providing cutting-edge aircraft maintenance, with a degree of safety that is unparalleled.  Honesty, value, and customer service are virtues by which we operate.  With superb technicians, backed by the worlds largest avionics suppliers, we believe that we've got what it takes to meet our goals.....  And yours. </p>
-        </div>
+        <Container>
+            <Row><h2 className="pt-4">About Flight-Deck Avionics</h2></Row>
+            <Row>
+                <Col className="pt-4" lg={4} md={12}>
+                    <Image src="https://via.placeholder.com/300" rounded />
+                </Col>
+                <Col className="pt-4" lg={8} md={12}>
+                    <p>Flight-Deck Avionics is Utah's premier FAA Certified Repair Station offering Avionics services and installation within the Salt Lake City/ Provo metropolitan area.  With fixed locations at SLC and PVU airports, and complete mobile services, we strive to offer the utmost in convenience to our clients.  The primary goal of Flight-Deck Avionics is to operate a successful business, providing cutting-edge aircraft maintenance, with a degree of safety that is unparalleled.  Honesty, value, and customer service are virtues by which we operate.  With superb technicians, backed by the worlds largest avionics suppliers, we believe that we've got what it takes to meet our goals.....  And yours. </p>
+                </Col>
+            </Row>
+            <Row>
+                <Col className="pt-4" lg={8} md={12}>
+                    <p>Flight-Deck Avionics is Utah's premier FAA Certified Repair Station offering Avionics services and installation within the Salt Lake City/ Provo metropolitan area.  With fixed locations at SLC and PVU airports, and complete mobile services, we strive to offer the utmost in convenience to our clients.  The primary goal of Flight-Deck Avionics is to operate a successful business, providing cutting-edge aircraft maintenance, with a degree of safety that is unparalleled.  Honesty, value, and customer service are virtues by which we operate.  With superb technicians, backed by the worlds largest avionics suppliers, we believe that we've got what it takes to meet our goals.....  And yours. </p>
+                </Col>
+                <Col className="pt-4" lg={4} md={12}>
+                    <Image src="https://via.placeholder.com/300" rounded />
+                </Col>
+            </Row>
+        </Container>
     )
 }
 
--- pages/services.js
+++ pages/services.js
@@ -1,5 +1,5 @@
 import styles from '../styles/Home.module.css'
-import Image from 'react-bootstrap/Image'
+import Card from 'react-bootstrap/Card'
 import {Container, Row, Col} from 'react-bootstrap/'
 
 
@@ -10,66 +10,68 @@ const services = () => {
 
             <Container>
                 <Row>
-                    <Col>
-                        <Image src="https://via.placeholder.com/150" roundedCircle />
-                    </Col>
-                    <Col>
-                        <Image src="https://via.placeholder.com/150" roundedCircle />
-                    </Col>
-                    <Col>
-                        <Image src="https://via.placeholder.com/150" roundedCircle />
-                    </Col>
-                    <Col>
-                        <Image src="https://via.placeholder.com/150" roundedCircle />
-                    </Col>
-                    <Col>
-                        <Image src="https://via.placeholder.com/150" roundedCircle />
-                    </Col>
-                    <Col>
-                        <Image src="https://via.placeholder.com/150" roundedCircle />
-                    </Col>
+                    <h3>Class Ratings:</h3>
+                    <Row>
+                        <Col md={12} lg={4}>
+                            <Card className={styles.card}>
+                                <Card.Img src="https://via.placeholder.com/300"/>
+                                <Card.ImgOverlay>
+                                    <Card.Title style={{bottom: 10}}>Instrument</Card.Title>
+                                </Card.ImgOverlay>
+                            </Card>
+                        </Col>
+                        <Col md={12} lg={4}>
+                            <Card className={styles.card}>
+                                <Card.Img src="https://via.placeholder.com/300"/>
+                                <Card.ImgOverlay>
+                                    <Card.Title>Radio</Card.Title>
+                                </Card.ImgOverlay>
+                            </Card>
+                        </Col>
+                        <Col md={12} lg={4}>
+                            <Card className={styles.card}>
+                                <Card.Img src="https://via.placeholder.com/300"/>
+                                <Card.ImgOverlay>
+                                    <Card.Title>Accessory</Card.Title>
+                                </Card.ImgOverlay>
+                            </Card>
+                        </Col>
+                    </Row>
                 </Row>
                 <Row>
-                    <Col>
-                        <Image src="https://via.placeholder.com/150" roundedCircle />
-                    </Col>
-                    <Col>
-                        <Image src="https://via.placeholder.com/150" roundedCircle />
-                    </Col>
-                    <Col>
-                        <Image src="https://via.placeholder.com/150" roundedCircle />
-                    </Col>
-                    <Col>
-                        <Image src="https://via.placeholder.com/150" roundedCircle />
-                    </Col>
-                    <Col>
-                        <Image src="https://via.placeholder.com/150" roundedCircle />
-                    </Col>
-                    <Col>
-                        <Image src="https://via.placeholder.com/150" roundedCircle />
-                    </Col>
-                    <Col>
-                        <Image src="https://via.placeholder.com/150" roundedCircle />
-                    </Col>
+                    <h3>Limited Ratings:</h3>
                 </Row>
                 <Row>
+                    <h3>Primary Services:</h3>
+                </Row>
+                <Row>
+                    <h3>Business Partners:</h3>
                     <Col>
-                        <Image src="https://via.placeholder.com/150" roundedCircle />
+                        <ul>
+                            <li>Bendix/King</li>
+                            <li>Universal Avionics</li>
+                            <li>L-3 Avionics</li>
+                            <li>Garmin International</li>
+                            <li>Avidyne Corporation</li>
+                            <li>Genesys Aerosystems Group (Chelton / Cobham / S-Tec)</li>
+                            <li>Northern Airborne Technology</li>
+                            <li>Technisonic</li>
+                            <li>AmSafe Aviation</li>
+                            <li>Aircraft Electronics Association</li>
+                        </ul>
                     </Col>
                     <Col>
-                        <Image src="https://via.placeholder.com/150" roundedCircle />
-                    </Col>
-                    <Col>
-                        <Image src="https://via.placeholder.com/150" roundedCircle />
-                    </Col>
-                    <Col>
-                        <Image src="https://via.placeholder.com/150" roundedCircle />
-                    </Col>
-                    <Col>
-                        <Image src="https://via.placeholder.com/150" roundedCircle />
-                    </Col>
-                    <Col>
-                        <Image src="https://via.placeholder.com/150" roundedCircle />
+                        <ul>
+                            <li>Rockwell Collins</li>
+                            <li>Aspen Avionics</li>
+                            <li>J.P. Instruments</li>
+                            <li>Mid-Continent Instruments and Avionics</li>
+                            <li>Sandel Avionics</li>
+                            <li>PS Engineering</li>
+                            <li>ACR/Artex</li>
+                            <li>FreeFlight Systems</li>
+                            <li>Cobham</li>
+                        </ul>
                     </Col>
                 </Row>
             </Container>
