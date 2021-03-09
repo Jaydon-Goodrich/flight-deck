90395f9536088bc929258230d2497e7bd4cd5b18
Jaydon Goodrich <jaydongoodrich@gmail.com>
2021-03-08 20:48:49 -0700

    vercel

--- config/index.js
+++ config/index.js
@@ -1,3 +1,4 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? 'http://localhost:3000' : 'website.here'
+export const server = dev ? 'http://localhost:3000' : 'https://flight-deck-avionics-f0qqnx33s-jaydon-goodrich.vercel.app'
+'
